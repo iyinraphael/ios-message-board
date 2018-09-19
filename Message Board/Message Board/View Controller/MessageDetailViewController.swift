647c1ec3de0e0b9234832b5909f85d176658f291
iyinraphael <iyinraphael@gmail.com>
2018-09-19 12:55:39 -0700

    storyboard set up

---
+++ Message Board/Message Board/View Controller/MessageDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MessageDetailViewController.swift
+//  Message Board
+//
+//  Created by Iyin Raphael on 9/19/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBAction func send(_ sender: Any) {
+    }
+    
+}
